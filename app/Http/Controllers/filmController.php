f9d4bd0c5553a6411827cfa1b028440837cdd113
rabiilfarakh <rabiilfarakh2816@gmail.com>
2024-02-22 17:03:17 +0100

    add front reservation

--- app/Http/Controllers/filmController.php
+++ app/Http/Controllers/filmController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\films;
-use App\Models\images;
+use App\Models\Film;
+use App\Models\Image;
 
 class filmController extends Controller
 {
     public function showFilm($id){
-        $film = films::findOrFail($id); 
-        $images = images::where('film_id', $id)->get();
+        $film = Film::findOrFail($id); 
+        $images = Image::where('film_id', $id)->get();
         return view('user.film',compact('film','images'));
     }
 }
--- resources/views/user/reservation.blade.php
+++ resources/views/user/reservation.blade.php
@@ -1,1 +1,52 @@
-<h1>reservation</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Document</title>
+    <style>
+        /* Styles CSS en ligne pour l'image */
+        .full-height {
+            height: 100vh; /* La hauteur de l'image prendra 100% de la hauteur de la vue */
+            object-fit: cover; /* Pour que l'image remplisse complètement le conteneur */
+            position: relative; /* Position relative pour permettre le positionnement absolu */
+        }
+
+        /* Superposition de couleur blanche */
+        .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%; /* Hauteur de l'overlay, ajustez-la selon vos besoins */
+            background-color: rgba(10, 10, 10, 0.271); /* Couleur blanche avec une opacité de 80% */
+        }
+    </style>
+</head>
+<body class="bg-black flex justify-between">
+
+    <div class="w-1/4 relative"> <!-- Ajout de position relative pour que l'overlay soit relatif à cette div -->
+        @foreach($images as $image)
+            <img src="{{ asset('img/' . $image->image . '.jpg') }}" class="full-height"> <!-- Ajout de la classe full-height à l'image -->
+            <div class="overlay"></div> <!-- Ajout de la div pour l'overlay -->
+        @endforeach
+    </div>
+
+    <div class="flex flex-col justify-center items-center w-3/4"> 
+        <div class="text-white text-4xl mb-4">Sélectionnez vos places</div> 
+        <div class="shadow-lg bg-white p-4 rounded-lg w-auto mt-5">
+            <div class="">
+                @for($i = 0; $i < $tailleSalle; $i++)
+                    @if($i % 10 == 0 && $i != 0)
+                        </div><div class="mt-2">
+                    @endif
+                    <i class="fas fa-couch text-gray-500 hover:text-green-500 text-2xl"></i>
+                @endfor
+            </div>
+        </div>
+    </div>
+</body>
+</html>
